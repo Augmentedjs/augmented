cc2f989821faac617168fce3977cd76c8e8c5c5f
m658761 <bob.warren@macys.com>
2016-05-24 13:14:01 -0700

    adding new tests

--- src/main/webapp/scripts/presentation/test/dialogSpec.js
+++ src/main/webapp/scripts/presentation/test/dialogSpec.js
@@ -12,5 +12,39 @@ define([
         dialogView
         */
 
+        describe('Given an instance of DialogView', function() {
+            var MyDialog = Augmented.Presentation.DialogView.extend({
+                name: "sample",
+                el: "#sandbox"
+            });
+            var view = null;
+
+            beforeEach(function() {
+                view = new MyDialog();
+            });
+
+            afterEach(function() {
+                view.remove();
+                view = null;
+            });
+
+            it('has a view that extends DialogView', function() {
+                expect(MyDialog).toBeDefined();
+            });
+
+            it('instance is an instance of DialogView', function() {
+                expect(view instanceof Augmented.Presentation.DialogView).toBeTruthy();
+            });
+
+            it('dialog can open', function() {
+                view.open();
+                expect(view.el.childNodes.length).toBeGreaterThan(0);
+            });
+
+            it('dialog can close', function() {
+                view.close();
+                expect(view.el.childNodes.length).toEqual(0);
+            });
+        });
 	});
 });
