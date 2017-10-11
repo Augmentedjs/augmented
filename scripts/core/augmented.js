f3450ce9a6e7153d1cba7f3b2584f12508811147
thedocbwarren <jedi.master@augmentedjs.com>
2017-10-11 08:39:09 -0700

    update docs

--- scripts/core/augmented.js
+++ scripts/core/augmented.js
@@ -5,7 +5,7 @@
 *
 * @requires Backbone.js
 * @module Augmented
-* @version 1.5.2
+* @version 1.5.3
 * @license Apache-2.0
 */
 (function(root, factory) {
@@ -54,7 +54,7 @@
   * The standard version property
   * @constant VERSION
   */
-  Augmented.VERSION = "1.5.2";
+  Augmented.VERSION = "1.5.3";
   /**
   * A codename for internal use
   * @constant codename
